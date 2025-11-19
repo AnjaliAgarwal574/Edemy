25a4d748c019181c60dd6a6aff845c7533ab576c
AnjaliAgarwal574 <anjaliagarwal2399@gmail.com>
2025-11-19 11:09:17 +0530

    changes02

--- server/server.js
+++ server/server.js
@@ -36,20 +36,46 @@
 //   console.log(`Server is running on port ${PORT}`);
 // })
 
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+// import express from 'express';
+// import cors from 'cors';
+// import 'dotenv/config';
 
-const app = express();
+// const app = express();
 
-// middleware
-app.use(cors());
-app.use(express.json());
+// // middleware
+// app.use(cors());
+// app.use(express.json());
 
-// simple API check route
-app.get('/', (req, res) => {
-    res.json({ message: "API working on Vercel 🚀" });
-});
+// // simple API check route
+// app.get('/', (req, res) => {
+//     res.json({ message: "API working on Vercel 🚀" });
+// });
 
-// export handler (important)
-export default app;
+// // export handler (important)
+// export default app;
+
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// Initialize Express
+const app = express()
+
+
+
+// Middlewares
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => res.send("API Working"))
+
+
+// Port
+const PORT = process.env.PORT || 5000
+
+// server listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
